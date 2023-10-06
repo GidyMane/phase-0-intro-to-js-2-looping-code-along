fa9ae0cd0c637a3d690271f37c5ef9519f9d3acf
GidyMane <muragegideon2000@gmail.com>
2023-10-06 19:17:36 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(names, eventName) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+
+function countDown(integer) {
+    if (integer < 0) {
+      console.log("Please provide a positive integer.");
+      return;
+    }
+  
+    for (let i = integer; i >= 0; i--) {
+      console.log(i);
+    }
+  }
